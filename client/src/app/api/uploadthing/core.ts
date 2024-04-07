876c7d8d48506a874999eacb69383ff441edac8c
Harsh <harshvardhan23.007@gmail.com>
2024-04-08 03:47:38 +0530

    build fixes

--- client/src/app/api/uploadthing/core.ts
+++ client/src/app/api/uploadthing/core.ts
@@ -7,13 +7,13 @@ const auth = (req: Request) => ({ id: "fakeId" });
  
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // .middleware(async ({ req }) => {
-    //   const user = await auth(req);
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
  
 
-    //   if (!user) throw new UploadThingError("Unauthorized");
-    //   return { userId: user.id };
-    // })
+      if (!user) throw new UploadThingError("Unauthorized");
+      return { userId: user.id };
+    })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
